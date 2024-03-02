ce2707d6a499a1dd7e2a792f56f4155be9d8bcab
Mike Hague <mlh5599@gmail.com>
2024-03-02 15:42:43 -0600

    Make tailscaleledPi a bit more efficient.

--- tailscaleledPi.sh
+++ tailscaleledPi.sh
@@ -7,36 +7,56 @@ if [[ $# -eq 0 ]]; then
 fi
 
 led_pin=$1
+
 is_connected=0
+
+last_check_millis=0
+last_blink_millis=0
 
 pigs w $led_pin 0
 
 # Loop indefinitely
 while true
 do
-    # Check tailscale daemon status
-    tailscale_status=$(tailscale status | grep "Tailscale is stopped." | wc -l)
-    # If tailscale is running, turn off the lights via pigpio, if not, turn them on
-    if [[ "$tailscale_status" -eq 1 ]]; then
-        # Lights should flash
-        is_connected=0
-    else
-        # Turn on the lights
-        is_connected=1
-    fi
-
-    if [[ "$is_connected" -eq 1 ]]; then
-        pigs w $led_pin 1
-    else
-        is_on=$(pigs r $led_pin)
-        if [[ "$is_on" -eq 1 ]]; then
-            pigs w $led_pin 0
+    curr_millis=$(date +%s%3N)
+    difference=`expr $curr_millis - $last_check_millis`
+    #echo "$difference"
+    if [[ $difference -gt 10000 ]]; then
+        #echo "Check status"
+        # Check tailscale daemon status
+        tailscale_status=$(tailscale status | grep "Tailscale is stopped." | wc -l)
+        # If tailscale is running, turn off the lights via pigpio, if not, turn them on
+        if [[ "$tailscale_status" -eq 1 ]]; then
+            # Lights should flash
+            is_connected=0
         else
-           pigs w $led_pin 1
+            # Turn on the lights
+            is_connected=1
+        fi
+        last_check_millis=$curr_millis
+
+
+        if [[ "$is_on" -ne 1 ]] && [[ "$is_connected" -eq 1 ]]; then
+            #echo "On"
+            pigs w $led_pin 1
+            is_on=1
         fi
     fi
 
+    if [[ "$is_connected" -ne 1 ]] then
+        if [[ "$is_on" -eq 1 ]]; then
+            #echo "Blink On"
+            pigs w $led_pin 0
+            is_on=0
+        else
+            #echo "Blink Off"
+            pigs w $led_pin 1
+            is_on=1
+        fi
+        last_blink_millis=$curr_millis
+    fi
 
-    # Sleep for .5 seconds
-    sleep .5
+
+    # Sleep for 1 seconds
+    sleep 1
 done
